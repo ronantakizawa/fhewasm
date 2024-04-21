5feaff57b66332616199eeee4095fa922f5e203d
Ronan Takizawa <71115970+ronantakizawa@users.noreply.github.com>
2024-04-20 18:53:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace seal;
 unique_ptr<SEALContext> context;
 unique_ptr<KeyGenerator> keygen;
 unique_ptr<PublicKey> public_key;
+unique_ptr<SecretKey> secret_key;
 unique_ptr<Encryptor> encryptor;
 unique_ptr<Evaluator> evaluator;
 unique_ptr<Decryptor> decryptor;
@@ -27,20 +28,58 @@ extern "C" {
         EncryptionParameters params(scheme_type::bgv);
         size_t poly_modulus_degree = 8192;
         params.set_poly_modulus_degree(poly_modulus_degree);
-        params.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 40, 40, 60}));
+        params.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, { 60, 40, 40, 60 }));
         params.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20));
 
         context = make_unique<SEALContext>(params);
         keygen = make_unique<KeyGenerator>(*context);
         public_key = make_unique<PublicKey>();
+        secret_key = make_unique<SecretKey>();
         keygen->create_public_key(*public_key);
+        *secret_key = keygen->secret_key();
 
         encryptor = make_unique<Encryptor>(*context, *public_key);
         evaluator = make_unique<Evaluator>(*context);
-        decryptor = make_unique<Decryptor>(*context, keygen->secret_key());
+        decryptor = make_unique<Decryptor>(*context, *secret_key);
         encoder = make_unique<BatchEncoder>(*context);
     }
 
+    EMSCRIPTEN_KEEPALIVE
+    const char* get_public_key() {
+        static std::string key_str; // Use static to prolong the life of key_str
+        std::stringstream ss;
+        public_key->save(ss);
+        key_str = ss.str();
+        return key_str.c_str();
+    }
+
+    EMSCRIPTEN_KEEPALIVE
+    const char* get_private_key() {
+        static std::string key_str; // Use static to prolong the life of key_str
+        std::stringstream ss;
+        secret_key->save(ss);
+        key_str = ss.str();
+        return key_str.c_str();
+    }
+    EMSCRIPTEN_KEEPALIVE
+    std::string serialize_encrypted_data(int id) {
+    // Check if the id exists in the cipher_storage
+    auto it = cipher_storage.find(id);
+    if (it == cipher_storage.end()) {
+        throw std::invalid_argument("Invalid ID: No encrypted data found with the given ID.");
+    }
+
+    // Retrieve the Ciphertext
+    const Ciphertext& encrypted = it->second;
+
+    // Serialize the Ciphertext
+    std::stringstream ss;
+    encrypted.save(ss);
+
+    // Convert the stringstream to a string and return
+    return ss.str();
+    }
+    
     EMSCRIPTEN_KEEPALIVE
     int encrypt_integer(int value) {
         vector<uint64_t> vec(encoder->slot_count(), 0);
@@ -83,7 +122,19 @@ extern "C" {
     }
 
     EMSCRIPTEN_KEEPALIVE
-    void free_ciphertext(int id) {
-        cipher_storage.erase(id);
+    void clear_all_data() {
+        // Clear the ciphertext storage map
+        cipher_storage.clear();
+        // Optionally reset the counter if you want to start from zero again
+        cipher_id_counter = 0;
     }
+    EMSCRIPTEN_KEEPALIVE
+    int get_private_key_length() {
+        static std::string key_str;
+        std::stringstream ss;
+        secret_key->save(ss);
+        key_str = ss.str();
+        return key_str.size();
+    }
+    
 }
